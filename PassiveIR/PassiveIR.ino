3803d15c86ca4c0976f83ff1547e6b044713bb74
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2018-04-03 18:49:40 -0400

    Created Passive IR project

---
+++ PassiveIR/PassiveIR.ino
@@ -0,0 +1,30 @@
+
+#include "HeepDeviceDefinitions.h"
+
+#define IRSENSE_PIN 14
+
+void InitializeControlHardware(){
+  pinMode(IRSENSE_PIN,INPUT);
+}
+
+int ReadIRSense(){
+  int currentSetting = digitalRead(IRSENSE_PIN);
+  SetControlValueByName("IRSense",currentSetting);
+  return currentSetting;
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+  InitializeControlHardware();
+  AddOnOffControl("IRSense",HEEP_OUTPUT,0);
+  StartHeep("PassiveIR", 7);
+
+}
+
+void loop()
+{
+  PerformHeepTasks();
+  ReadIRSense();
+}
